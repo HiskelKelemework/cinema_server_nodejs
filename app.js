f9fde0137dbde6e9589523f5d3cc0dccdeff1e87
Hiskel Kelemework <dawitgizaw51@gmail.com>
2019-08-28 14:37:15 +0300

    trying out connecting with mongodb atlas from heroku

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+let mongoose = require('mongoose');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+mongoose.connect('mongodb+srv://atlas_admin_1:ATLAS@admin1@123@cluster0-emqje.mongodb.net/test?retryWrites=true&w=majority',
+{useNewUrlParser: true});
+
+var db = mongoose.connection;
+var connected = false;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('connected to test db');
+    connected = true;
+    // dummy();
+});
+
+const Info = mongoose.model('Info', mongoose.Schema({
+    name: String,
+    age: Number
+}));
+app.use((req, res, next) => {
+    if (!connected) res.json({success: false, message: 'no db connection'});
+    else next();
+});
+
+app.get('/', (req, res) => {
+    Info.find({}).lean().exec((err, result) => {
+        if (err) res.json({success: false, message: 'something went wrong'});
+        else if (!result || result.length === 0) res.json({success: false, message: 'no items found'});
+        else res.json({success: true, data: result});
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
